ada329d8026b6245a3f01d5214e99988b11a2488
Christian Ziegler <ziegler.christian@kabelmail.de>
2019-10-27 21:30:52 +0100

    finish users api

--- api/users.js
+++ api/users.js
@@ -6,7 +6,7 @@ class Users {
     this.axios = axios;
   }
 
-  createUser(params) {
+  async createUser(params) {
     if (!params.username || !params.password || !params.pin) {
       throw Error('createUser must contain username, password, pin');
     }
@@ -23,7 +23,7 @@ class Users {
     return connectionService(this.connectionDetails, this.axios, 'users/create/user', params);
   }
 
-  loginUser(params) {
+  async loginUser(params) {
     if (!params.username || !params.password || !params.pin) {
       throw Error('createUser must contain username, password, pin');
     }
@@ -40,48 +40,139 @@ class Users {
     return connectionService(this.connectionDetails, this.axios, 'users/login/user', params);
   }
 
-  logoutUser(params) {
-    console.log('logoutUser');
+  async logoutUser(params) {
+    if (!params.session) {
+      throw Error('logoutUser missing parameter session');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/logout/user', params);
   }
 
-  unlockUser(params) {
-    console.log('unlockUser');
+  async unlockUser(params) {
+    if (!params.pin) {
+      throw Error('unlockUser missing parameter pin');
+    }
+    if (!params.mining) {
+      throw Error('unlockUser missing parameter mining');
+    }
+    if (!params.notifications) {
+      throw Error('unlockUser missing parameter notifications');
+    }
+    if (!params.staking) {
+      throw Error('unlockUser missing parameter staking');
+    }
+    if (!params.transactions) {
+      throw Error('unlockUser missing parameter transactions');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/unlock/user', params);
   }
 
-  lockUser() {
-    console.log('lockUser');
+  async lockUser(params) {
+    if (!params.mining) {
+      throw Error('lockUser missing parameter mining');
+    }
+    if (!params.notifications) {
+      throw Error('lockUser missing parameter notifications');
+    }
+    if (!params.staking) {
+      throw Error('lockUser missing parameter staking');
+    }
+    if (!params.transactions) {
+      throw Error('lockUser missing parameter transactions');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/lock/user', params);
   }
 
-  listAccounts() {
-    console.log('listAccounts');
+  async getStatus() {
+    const params = {};
+    return connectionService(this.connectionDetails, this.axios, 'users/get/status', params);
   }
 
-  listAssets() {
-    console.log('listAssets');
+  listAssets(params) {
+    if (!params.genesis) {
+      throw Error('listAssets missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listAssets missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/assets', params);
   }
 
-  listItems() {
-    console.log('listItems');
+  async listAccounts(params) {
+    if (!params.genesis) {
+      throw Error('listAccounts missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listAccounts missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/accounts', params);
   }
 
-  listNames() {
-    console.log('listNames');
+  async listItems(params) {
+    if (!params.genesis) {
+      throw Error('listItems missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listItems missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/items', params);
   }
 
-  listNamespaces() {
-    console.log('listNamespaces');
+  async listNames(params) {
+    if (!params.session) {
+      throw Error('listNames missing parameter genesis');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/names', params);
   }
 
-  listNotifications() {
-    console.log('listNotifications');
+  async listNamespaces(params) {
+    if (!params.session) {
+      throw Error('listNamespaces missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listNamespaces missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/namespaces', params);
   }
 
-  listTokens() {
-    console.log('listTokens');
+  async listNotifications(params) {
+    if (!params.genesis) {
+      throw Error('listNotifications missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listNotifications missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/notifications', params);
   }
 
-  listTransactions() {
-    console.log('listTransactions');
+  async listTokens(params) {
+    if (!params.genesis) {
+      throw Error('listTokens missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listTokens missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/tokens', params);
+  }
+
+  async listTransactions(params) {
+    if (!params.genesis) {
+      throw Error('listTransactions missing parameter genesis');
+    }
+    if (!params.username) {
+      throw Error('listTransactions missing parameter username');
+    }
+
+    return connectionService(this.connectionDetails, this.axios, 'users/list/transactions', params);
   }
 }
 
